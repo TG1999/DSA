2a733cab53fb5e33487d6ce287f703af594e97d4
tushar goel <tushar.goel.dav@gmail.com>
2019-09-29 15:45:15 +0530

    changes
    Signed-off-by: tushar goel <tushar.goel.dav@gmail.com>

--- 2nd_Lecture_Array/lcs2.cc
+++ 2nd_Lecture_Array/lcs2.cc
@@ -3,10 +3,11 @@ using namespace std;
 int main()
 {   //Naive Solution with N^3 complexity
     // Algorithm used
-    //1) Find a cummulative sum array
-    //2) Using three nested loops 
-    //3) One (i) going from 0 to n-1 then other(j) going from i to n-1 then one(k) going from i to j
-    //4) Pick the subarray with greatest sum :)
+    //1) Find a cummulative sum array using cumsum[i]=cumsum[i-1] + arr[i] here if i=0 then cumsum[i]=arr[0]
+    //2) Using two nested loops 
+    //3) One (i) going from 0 to n-1 then other(j) going from i to n-1 
+    //4) then use i,j in this way to get sum from i to j cusrum=cumsum[j]-cumsum[i-1] and if i=0 then cursum=cumsum[j]
+    //5) then give the maximum as answer :)
     int arr[10];
     int n ;
     cin >> n;
